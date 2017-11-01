2767bdb4bdcf5b9599491b1dc92304fb0e67b226
Unknown <brain.agam@gmail.com>
2017-11-01 15:20:52 +0100

    selection sort in python

---
+++ selectionsort/selection_sort.py
@@ -0,0 +1,18 @@
+"""Selection sort in python"""
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+def selection_sort(v):
+    for i in range(len(v)):
+        mini = min(v[i:])
+        min_index = v[i:].index(mini)
+        v[i + min_index], v[i] = v[i], mini
+
+def main():
+    """type an unsorted vector"""
+    v = [4,2,3,1]
+    selection_sort(v)
+    print(v)
+
+if __name__ == '__main__':
+    main()
